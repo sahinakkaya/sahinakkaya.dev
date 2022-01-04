ca227f8053d8b0259eab853abe01d0cdad9bec6a
Asocia <Asocia@users.noreply.github.com>
2022-01-04 16:51:06 +0000

    jekyll build from Action 6512a767400bc288237d57fae07d0d300dcd732a

---
+++ README.md
@@ -0,0 +1,5 @@
+This is the repository for the source code of my website hosted at [sahinakkaya.dev](https://sahinakkaya.dev).
+I am using [Jekyll](https://jekyllrb.com) to generate the content.
+```bash
+bundle exec jekyll build
+```
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "First blog post",
+        "excerpt":"Hello, World!* So here I am and welcome to my first blog. Having a personal space on the Internet has been a dream for me for years and I am happy that it finally have come true. You might think that I could sign-up for a social media platform and...","categories": [],
+        "tags": [],
+        "url": "/2021/12/24/first-blog-post.html",
+        "teaser": null
+      },{
+        "title": "Stop cat-pipe'ing, You Are Doing It Wrong!",
+        "excerpt":"cat some_file | grep some_pattern I’m sure that you run a command something like above at least once if you are using terminal. You know how cat and grep works and you also know what pipe (|) does. So you naturally combine all of these to make the job done....","categories": [],
+        "tags": [],
+        "url": "/2022/01/01/stop-cat-pipeing.html",
+        "teaser": null
+      }]
