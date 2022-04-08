464ea85348d6fe9b60d7bc048cebd91a9566f15d
Asocia <Asocia@users.noreply.github.com>
2022-04-08 15:59:48 +0000

    jekyll build from Action bb25851379fc54f4d7570bbdc759bcd9e67999ed

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -40,4 +40,10 @@ var store = [{
         "tags": ["linux","grub","partition","uefi"],
         "url": "/2022/03/03/never-get-trapped-in-grub-rescue-again.html",
         "teaser": null
+      },{
+        "title": "Confession Time",
+        "excerpt":"Last week, I received an email from LetsEncrypt reminding me to renew my certificates. I forgot to renew it and the certificate expired. Now I can’t send or receive any emails. If you send me email in the last week and wonder why I didn’t respond, this is the reason....","categories": [],
+        "tags": ["ssl"],
+        "url": "/2022/04/08/confession-time.html",
+        "teaser": null
       }]
